80acbadc6fcb6b4f04aa90d0fc42b85762b30be7
LopezRussiAlvaroFelipe <aflopez67@ucatolica.edu.co>
2022-11-28 18:39:22 -0500

    v 1.0.1

--- src/main/java/com/mycompany/uniapp/model/AspiranteDto.java
+++ src/main/java/com/mycompany/uniapp/model/AspiranteDto.java
@@ -4,12 +4,69 @@
  */
 package com.mycompany.uniapp.model;
 
+import java.io.Serializable;
+import java.util.Objects;
+
 /**
  *
  * @author santi
  */
-public class AspiranteDto {
-    int hola;
-   
+public class AspiranteDto implements Serializable {
+    
+   private String usuario;
+   private String password;
 
+    /**
+     * @return the usuario
+     */
+    public String getUsuario() {
+        return usuario;
+    }
+
+    /**
+     * @param usuario the usuario to set
+     */
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    /**
+     * @return the password
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * @param password the password to set
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    
+    
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        return hash;
+    }
+    
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final AspiranteDto other = (AspiranteDto) obj;
+        if (!Objects.equals(this.usuario, other.usuario)) {
+            return false;
+        }
+        return Objects.equals(this.password, other.password);
+    }
 }
